bb96771faa5d0b19d771ff7919e2f7bb6167fc97
Brenda Ombeyi <brendaotieno2001@gmail.com>
2024-07-10 12:33:07 +0300

    .results.json index.js

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 function scuberGreetingForFeet(){
   // Write your code here!
-}
+  if(distance <= 400){
+    return 'This one is on me!'
 
+}else if (distance > 400 && distance < 2000){
+     return 'That will be twenty bucks.'
+}else if (distance >= 2000 && distance <= 2500){
+  return 'I will be gladly take your thirty bucks.'
+}else if(distance > 2500){
+   return 'No can do'
+}
+distance(2501);
+}
 function ternaryCheckCity(){
   // Write your code here!
+  if(City === 'NYC'){
+    return  "Ok, sounds good."
+  }else {
+    return 'No go.'
+  }
 }
-
 function switchOnCharmFromTip(){
   // Write your code here!
+  if(tip === generous){
+    return 'Thank you so much'
+} else if (tip === 'not as generous'){
+  return 'Thank you. '
+}else {
+  return 'Bye'
+}
 }
